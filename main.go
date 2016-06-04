d4bc3efe1b3d00fbe4bdfd75055b965ad8ae085b
Guilherme Souza <guivideojob@gmail.com>
2016-06-04 09:27:18 -0300

    Fixing package import

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/guilhermef/go-shortener/handler"
 )
 
 const addr = "localhost:12345"
 
 func main() {
-	err := http.ListenAndServe(addr, &RedirectHandler{})
+	err := http.ListenAndServe(addr, &handler.RedirectHandler{})
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
